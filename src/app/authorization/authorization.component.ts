670b289b7fd862f6c5633b5dbd92fcf8d713667a
feartheoldblood1 <p_lobachev@inbox.ru>
2022-06-06 01:13:57 +0300

    update

--- src/app/authorization/authorization.component.ts
+++ src/app/authorization/authorization.component.ts
@@ -1,7 +1,11 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { map, Observable } from 'rxjs';
 import {User} from '../api/models/user';
+import { UserWithRelations } from '../api/models/user-with-relations';
 import {UserControllerService} from '../api/services/user-controller.service';
+import { StrictHttpResponse } from '../api/strict-http-response';
 
 @Component({
   selector: 'app-authorization',
@@ -13,6 +17,8 @@ import {UserControllerService} from '../api/services/user-controller.service';
 export class AuthorizationComponent implements OnInit {
   form: any;
   users: User[] = [];
+  router: any;
+  profilePage: string = '';
   constructor(private userService: UserControllerService) {
       
   }
@@ -22,6 +28,7 @@ export class AuthorizationComponent implements OnInit {
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
    })
+   
   }
   get _email() {
     return this.form.get('email')
@@ -30,13 +37,16 @@ export class AuthorizationComponent implements OnInit {
     return this.form.get('password')
   }
   onSubmit(): void {
-    console.log(this.getUsers());
+    this.getUsers()
   } 
-   getUsers() {
-    let isAuth: boolean; 
-    return this.userService
-      .findByNamePass$Response({email: this._email.value,password:this._password.value})
-    
-  }
+   getUsers(): void {
+     let userData=this.userService.findByNamePass({email: this._email.value,
+       password:this._password.value}).subscribe((users)=>(this.goToProfilePage('/registration', users)));
+ }
+  goToProfilePage(pageName:string, userData:UserWithRelations[]):void {
+    //this.router.navigate([`${pageName}`]);
+    let usr = userData[0].name;
+    this.profilePage="<span>Имя: </span> <p [innerHTML]=usr></p>"
 
+  }
 }
