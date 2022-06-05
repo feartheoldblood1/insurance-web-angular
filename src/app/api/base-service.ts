ed0987d58b52cc6042bb7f8bfc66de64fc0cbe27
feartheoldblood1 <p_lobachev@inbox.ru>
2022-06-06 01:15:26 +0300

    update

--- src/app/api/base-service.ts
+++ src/app/api/base-service.ts
@@ -15,7 +15,7 @@ export class BaseService {
   ) {
   }
 
-  private _rootUrl: string = '';
+  private _rootUrl: string = 'http://192.168.0.104:3000';
 
   /**
    * Returns the root url for all operations in this service. If not set directly in this
