272414e61198ced6b1a07641d3fcc297fd89bf74
feartheoldblood1 <p_lobachev@inbox.ru>
2022-06-15 17:01:05 +0300

    update

--- src/app/osago-form/osago-form.component.ts
+++ src/app/osago-form/osago-form.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, MaxLengthValidator } from '@angular/forms';
+export type driver  = {
+  id:number,
+  age: number
+  experience: number,
+  withoutAccident:number
+}
 
 @Component({
   selector: 'app-osago-form',
   templateUrl: './osago-form.component.html',
   styleUrls: ['./osago-form.component.css']
 })
+
+
 export class OsagoFormComponent implements OnInit {
   form:any;
   constructor() { }
   web3: any;
   accounts: Array<String> | undefined;
   isClicked: boolean = false;
+  isDriverClicked: boolean = false;
+   iterator:number = 2;
   drivers: {id:number, age:number, experience: number, withoutAccident:number}[] = [
-    { id: 1, age: 18, experience: 5, withoutAccident:10},
+    { id: 1, age:18, experience: 1, withoutAccident:1}
     
   ];
+  isBuyBtnClicked:boolean =false;
   formulaAnswer:number = 0;
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -23,10 +34,35 @@ export class OsagoFormComponent implements OnInit {
       power: new FormControl(null),
       period: new FormControl(null),
       region: new FormControl(null),
+      age: new FormControl(null),
+      experience: new FormControl(null),
+      withoutAccident: new FormControl(null),
   } )
   }
+
+  get _age():string {
+      return this.form.get('age')?.value
+  }
+  get _experience():string {
+    return this.form.get('experience')?.value
+}
+get _withoutAccident():string {
+    return this.form.get('withoutAccident')?.value
+}
+  
   addUser():void {
-    this.drivers.push({ id: 1, age: 18, experience: 5, withoutAccident:10})
+    this.drivers.push({ id: this.iterator++, age:18, experience: 1, withoutAccident:1})
+  }
+  findMax(drivers: driver []): number {
+    return  drivers.reduce((acc, curr) => acc.withoutAccident > curr.withoutAccident ? acc : curr).withoutAccident;
+  }
+  updateDrivers(drivers: driver[]):number {
+    let maxFirstWA:number = this.findMax(this.drivers);
+    if(maxFirstWA >= Number(this._withoutAccident))
+      return maxFirstWA;
+    else {
+      return Number(this._withoutAccident);
+    }
   }
   get _region():string {
     return this.form.get('region')?.value;
@@ -40,15 +76,31 @@ export class OsagoFormComponent implements OnInit {
   get _period():string {
     return this.form.get('period')?.value;
   }
-  getSum(transportType: number, power: number, period: number):string {
-    this.formulaAnswer = transportType*power*period;
-    return this.formulaAnswer.toString();
+
+  getSum(transportType: number, power: number, period: number, region:number, maxYearWithoutAccident:number):number {
+    let mainPart:number = region*transportType*power*period;
+    if (this.isDriverClicked) {
+      this.formulaAnswer = mainPart*maxYearWithoutAccident;
+    } else {
+      this.formulaAnswer = mainPart;
+    }
+    return this.formulaAnswer;
+  
   }
+  onBuyBtn():void { 
+    this.isBuyBtnClicked = !this.isBuyBtnClicked;
+   }
+
   onSubmit():void {
-    this.getSum(Number(this._period),Number(this._power),Number(this._transportType)*
-    Number(this._region));
+    let maxYearWithoutAccident = this.updateDrivers(this.drivers)
+    this.getSum(Number(this._transportType), Number(this._power), Number(this._period), Number(this._region), maxYearWithoutAccident)
+  }
+  toggleDrivers(): void {
+    this.isDriverClicked = !this.isDriverClicked;
+ 
   }
   toggleFormula():void {
     this.isClicked = true;
   }
+  
 }
